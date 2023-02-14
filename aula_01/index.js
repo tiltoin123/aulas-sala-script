8554059d035f46e80559ba29fe19cf7c51745462
tiltoin123 <italohttp25@gmail.com>
2023-02-14 20:19:14 -0300

    primeira rota

--- aula_01/index.js
+++ aula_01/index.js
@@ -1,1 +1,11 @@
 const express = require("express")
+const app = express();
+const port = 8080;
+
+app.listen(port);
+
+console.log("funciona")
+
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage');
+  });
