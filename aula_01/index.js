1e77dd5feadee1402cf2323943795072c0643746
tiltoin123 <italohttp25@gmail.com>
2023-02-15 21:56:50 -0300

    uma rota feita

--- aula_01/index.js
+++ aula_01/index.js
@@ -4,8 +4,11 @@ const port = 8080;
 
 app.listen(port);
 
-console.log("funciona")
-
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage');
+app.get('/',  ()=> {
+        console.log('deu certo')
   });
+let nome = 'batata'
+app.get('/testeHTML',(req,res)=>{
+  res.send("<h1 style='color:blue;text-align:center;'> tum tum tum tum tum tum tum tum pa</h1>");
+  console.log("alguma coisa")
+} );
