0524c456e384362325707a455ec2acdf31b3725c
tiltoin123 <italohttp25@gmail.com>
2023-03-07 19:07:43 -0300

    comandos basicos de sql

--- sql aula 1.sql
+++ sql aula 1.sql
@@ -1,3 +1,14 @@
 create database ProgSCRIPT;
 go
 use ProgSCRIPT;
+
+create table alunos(
+	id int primary key ,
+	nome varchar(255),
+	idade int
+)
+
+SELECT *
+FROM sys.tables;
+
+EXEC sp_help 'dbo.alunos';  
