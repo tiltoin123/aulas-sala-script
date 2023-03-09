23d4a65db70fd0adfecbce4984fd739d75465c90
tiltoin123 <italohttp25@gmail.com>
2023-03-08 21:09:13 -0300

    alteração sql

--- sql aula 1.sql
+++ sql aula 1.sql
@@ -14,8 +14,12 @@ FROM sys.tables;
 EXEC sp_help 'dbo.alunos';  
 
 insert into alunos (id,nome,idade) values(01,'mirosmar arioswaldo dragutino', 23)
+
 insert into alunos (id,nome,idade) values(02,'vladislau pereira bananeira', 123),
 (03,'jagutino lambari bananeira', 12)
+
 select * from alunos;
+
 select * from alunos where idade >= 20;
+
 update alunos set idade = 20 where id =3;
